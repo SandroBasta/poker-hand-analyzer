a3c26fd50193c256f2314dfd8ec8c86390bdadd1
Sandro Basta <sandrobasta@outlook.com>
2020-09-05 22:35:39 +0200

    Traget blank footer

--- src/resources/views/partial/footer.blade.php
+++ src/resources/views/partial/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="page-footer font-small">
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="http://sandrobasta.com/">sandrobasta.com</a>
+        <a target="_blank" href="http://sandrobasta.com/">sandrobasta.com</a>
     </div>
     <!-- Copyright -->
 </footer>
